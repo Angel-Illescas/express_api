034c626789582b032038af3ec29ec24ac2195e5d
Angel Illescas <angelalfonso.illescas@gmail.com>
2022-04-24 01:04:18 -0500

    Quinto ejercicio

---
+++ app.js
@@ -0,0 +1,59 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
+
+// HTTP METHODS
+
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorer GET ALL request ${new Date()}`)
+    const explorer1 ={id: 1, name: "Angel1"}
+    const explorer2 ={id: 2, name: "Angel2"}
+    const explorer3 ={id: 3, name: "Angel3"}
+    const explorer4 ={id: 4, name: "Angel4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
+// Crea un endpoint que te regrese un explorer mediando un ID
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorers with id ${req.params.id}`)
+    const explorer = { id:1, name: "Angel"}
+    res.status(200).json(explorer)
+})
+
+// Crea un endpoint que se encargue de crear un explorer
+
+app.post('/v1/explorers', (req, res) =>{
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body // Param del cliente
+    res.status(201).json({message: "Created"})
+})
+
+// Crea un endpoint que se encargue de actualizar un explorer
+
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body // Param. del cliente
+    res.status(200).json({message: "Updated!!"})
+})
+
+// Crea un endpoint para eliminar un explorer
+
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELATE request ${new Date()}`)
+    console.log(`Delate explorer with id ${req.params.id}`)
+    const requestBody = req.body // Param. del cliente
+    res.status(200).json({message: "Delated"})
+})
